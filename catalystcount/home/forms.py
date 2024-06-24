3f8e7dcfc70d55281ecc32342e8623e2e02450ce
Shadabmulla07 <56515975+Shadabmulla07@users.noreply.github.com>
2024-06-24 21:40:57 +0530

    Delete catalystcount directory

--- catalystcount/home/forms.py
+++
@@ -1,3 +0,0 @@
-from django import forms  
-class excelForm(forms.Form):    
-    file      = forms.FileField() # for creating file input  
--- catalystcount/static/js/style.js
+++
@@ -1,7 +0,0 @@
-var msgbox=document.querySelector('.msg')
-if (msgbox){
-    console.log("hiiiiiiiiiiiiii");
-    setTimeout(function(){
-        msgbox.style.display='none';
-    },2000);
-}
