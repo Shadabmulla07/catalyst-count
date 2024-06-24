1d1c063a3215af11a278c8cd6f17d96e3a7d6b85
Shadabmulla07 <56515975+Shadabmulla07@users.noreply.github.com>
2024-06-24 21:49:11 +0530

    Add files via upload

--- catalystcount/home/models.py
+++ catalystcount/home/models.py
@@ -15,19 +15,3 @@ class CoreUser(models.Model):
 
     class Meta:
         db_table='coreuser'
-
-class ExcelData(models.Model):
-    companyid=models.IntegerField()
-    name=models.CharField(max_length=200)
-    domain=models.CharField(max_length=200)
-    yearfounded=models.CharField(max_length=10)
-    industy=models.CharField(max_length=200)
-    sizerange=models.CharField(max_length=10)
-    locality=models.CharField(max_length=200)
-    county=models.CharField(max_length=200)
-    linkedinurl=models.CharField(max_length=200)
-    currentemployee=models.IntegerField()
-    totalemployee=models.IntegerField()
-
-    class Meta:
-        db_table='exceldata'
